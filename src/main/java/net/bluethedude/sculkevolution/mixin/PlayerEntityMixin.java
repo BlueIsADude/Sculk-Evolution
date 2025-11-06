805989aa431cfee535e9958f4dd7f683833df268
BlueIsADude <blueisdude@yahoo.com>
2025-11-05 18:20:59 -0600

    Updated Sickle sweeping attack mixin

--- src/main/java/net/bluethedude/sculkevolution/mixin/PlayerEntityMixin.java
+++ src/main/java/net/bluethedude/sculkevolution/mixin/PlayerEntityMixin.java
@@ -1,7 +1,7 @@
 package net.bluethedude.sculkevolution.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import net.bluethedude.sculkevolution.item.SculkItems;
+import net.bluethedude.sculkevolution.item.custom.SickleItem;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
     @ModifyVariable(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getItem()Lnet/minecraft/item/Item;", ordinal = 1), name = "bl4")
-    private boolean changeIfToDoSweeping(boolean original, @Local(name = "itemStack") ItemStack itemStack) {
-        if (itemStack.isOf(SculkItems.SICKLE)) {
+    private boolean canSweepAttack(boolean original, @Local(name = "itemStack") ItemStack itemStack) {
+        if (itemStack.getItem() instanceof SickleItem) {
             return true;
         }
         return original;
